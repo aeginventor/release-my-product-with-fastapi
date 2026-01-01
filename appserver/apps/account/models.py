105d8291f6a45d5338f83795120674a8da855f1e
San Kim <aeginventor@gmail.com>
2026-01-01 23:18:17 +0900

    feat: User 및 OAuthAccount 데이터 모델 정의

---
+++ appserver/apps/account/models.py
@@ -0,0 +1,74 @@
+from datetime import datetime, timezone
+from sqlmodel import SQLModel, Field, Relationship, func, Column, AutoString
+from pydantic import EmailStr, AwareDatetime
+from sqlalchemy import UniqueConstraint
+from sqlalchemy_utc import UtcDateTime
+
+class User(SQLModel, table=True):
+    __tablename__ = "users"
+    __table_args__ = (
+        UniqueConstraint("email", name="uq_email"),
+    )
+
+    id: int = Field(default=None, primary_key=True)
+    username: str = Field(unique=True, max_length=40, decription="사용자 계정 ID")
+    email: EmailStr = Field(max_length=128, description="사용자 이메일")
+    display_name: str = Field(max_length=40, description="사용자 표시 이름")
+    password: str = Field(max_length=28, description="사용자 비밀번호")
+    is_host: bool = Field(default=False, description="사용자가 호스트인지 여부")
+
+    oauth_accounts: list["OAuthAccount"] = Relationship(back_populates="user")
+
+    created_at: AwareDatetime = Field(
+        default=None,
+        nullable=False,
+        sa_type=UtcDateTime,
+        sa_column_kwargs={
+            "server_default": func.now(),
+        },
+    )
+    updated_at: AwareDatetime = Field(
+        default=None,
+        nullable=False,
+        sa_type=UtcDateTime,
+        sa_column_kwargs={
+            "server_default": func.now(),
+            "onupdate": lambda: datetime.now(timezone.utc),
+        }
+    )
+
+class OAuthAccount(SQLModel, table=True):
+    __tablename__ = "oauth_accounts"
+    __table_args__ = (
+        UniqueConstraint(
+            "provider",
+            "provider_account_id",
+            name="uq_provider_provider_account_id",
+        ),
+    )
+
+    id: int = Field(default=None, priamry_key=True)
+
+    provider: str = Field(max_length=10, decription="OAuth 제공자")
+    provider_account_id: str = Field(max_length=128, decription="OAuth 제공자 계정 ID")
+
+    user_id: int = Field(foreign_key="user.id")
+    user: User = Relationship(back_populates="oauth_accounts")
+
+    created_at: AwareDatetime = Field(
+        default=None,
+        nullable=False,
+        sa_type=UtcDateTime,
+        sa_column_kwargs={
+            "server_default": func.now(),
+        },
+    )
+    updated_at: AwareDatetime = Field(
+        default=None,
+        nullable=False,
+        sa_type=UtcDateTime,
+        sa_column_kwargs={
+            "server_default": func.now(),
+            "onupdate": lambda: datetime.now(timezone.utc),
+        }
+    )
